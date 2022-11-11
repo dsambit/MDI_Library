d42baa857c6b69f77af014905d51f949a0807ce0
Sambit Das <dsambit@umich.edu>
2022-11-11 10:25:24 -0500

    update

--- driverTestDFTFEPlugin/testcxxplugin/driver_plug_cxx/driver_plug_cxx.cpp
+++ driverTestDFTFEPlugin/testcxxplugin/driver_plug_cxx/driver_plug_cxx.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <string.h>
 #include <cstdlib>
+#include <iomanip>
 #include "mdi.h"
 
 
@@ -25,37 +26,130 @@ int code_for_plugin_instance(void* mpi_comm_ptr, MDI_Comm mdi_comm, void* class_
   if ( MDI_Recv(engine_name, MDI_NAME_LENGTH, MDI_CHAR, mdi_comm) != 0 ) {
     mpi_error("MDI_Recv returned non-zero exit code.");
   }
- /*
-  // Get the number of atoms
-  int natoms;
-  if ( MDI_Send_command("<NATOMS", mdi_comm) != 0 ) {
-    mpi_error("MDI_Send_command returned non-zero exit code.");
-  }
-  if ( MDI_Recv(&natoms, 1, MDI_INT, mdi_comm) != 0 ) {
-    mpi_error("MDI_Recv returned non-zero exit code.");
-  }
 
-  // Broadcast the number of atoms to all ranks within this plugin instance
-  MPI_Bcast(&natoms, 1, MPI_INT, 0, mpi_comm);
+  //Send natoms
+  int natoms=16;
+  MDI_Send_command(">NATOMS", mdi_comm);
+  MDI_Send(&natoms, 1, MDI_INT, mdi_comm);
 
-  // Get the nuclear coordinates
-  double* coords = new double [3*natoms];
-  if ( MDI_Send_command("<COORDS", mdi_comm) != 0 ) {
-    mpi_error("MDI_Send_command returned non-zero exit code.");
-  }
-  if ( MDI_Recv(coords, 3*natoms, MDI_DOUBLE, mdi_comm) != 0 ) {
-    mpi_error("MDI_Recv returned non-zero exit code.");
-  }
+  //Send elements
+  int elements[natoms]={13,28,13,28,13,28,13,28,13,28,13,28,13,28,13,28};
+  MDI_Send_command(">ELEMENTS", mdi_comm);
+  MDI_Send(&elements, natoms, MDI_INT, mdi_comm);
 
-  // Send the nuclear coordinates
-  if ( MDI_Send_command(">COORDS", mdi_comm) != 0 ) {
-    mpi_error("MDI_Send_command returned non-zero exit code.");
-  }
-  if ( MDI_Send(coords, 3*natoms, MDI_DOUBLE, mdi_comm) != 0 ) {
-    mpi_error("MDI_Send returned non-zero exit code.");
-  }
-  delete[] coords;
-  */
+  //Send cell
+  double cell[9];
+  cell[0]=10.9146877116;cell[1]=0.0;cell[2]=0.0;
+  cell[3]=0.0;cell[4]=10.9146877116;cell[5]=0.0;
+  cell[6]=0.0;cell[7]=0.0;cell[8]=10.9146877116;  
+  MDI_Send_command(">CELL", mdi_comm);
+  MDI_Send(cell, 9, MDI_DOUBLE, mdi_comm);
+
+  //Send dimensions
+  int dimensions[3];
+  dimensions[0]=2;//periodic
+  dimensions[1]=2;//periodic
+  dimensions[2]=2;//periodic
+  MDI_Send_command(">DIMENSIONS", mdi_comm);
+  MDI_Send(dimensions, 3, MDI_INT, mdi_comm);
+
+  //Send MP grid
+  int mpgrid[3];
+  mpgrid[0]=1;
+  mpgrid[1]=1;
+  mpgrid[2]=1;
+  MDI_Send_command(">MONKHORST-PACK_NPOINTS", mdi_comm);
+  MDI_Send(mpgrid, 3, MDI_INT, mdi_comm);  
+
+  //Send MP shift
+  double mpshift[3];
+  mpshift[0]=0;
+  mpshift[1]=0;
+  mpshift[2]=0;
+  MDI_Send_command(">MONKHORST-PACK_SHIFT", mdi_comm);
+  MDI_Send(mpshift, 3, MDI_DOUBLE, mdi_comm);  
+
+  //Send spin polarization
+  int spin=0;
+  MDI_Send_command(">SPIN_POLARIZATION", mdi_comm);
+  MDI_Send(&spin, 1, MDI_INT, mdi_comm);
+
+  //
+  //Send cartesian coordinates, origin at cell corner
+  //
+
+  //initialize with fractional and then convert to cartesian
+  double coordsfrac[3*natoms]={0.000000000000,0.000000000000,0.000000000000,
+                                      0.250000000000,0.250000000000,0.250000000000,
+                                      0.000000000000,0.000000000000,0.500000000000,
+                                      0.250000000000,0.250000000000,0.750000000000,
+				                              0.000000000000,0.500000000000,0.000000000000,
+                                      0.250000000000,0.750000000000,0.250000000000,
+                                      0.000000000000,0.500000000000,0.500000000000,
+                                      0.250000000000,0.750000000000,0.750000000000,
+                                      0.500000000000,0.000000000000,0.000000000000,
+                                      0.750000000000,0.250000000000,0.250000000000,
+                                      0.500000000000,0.000000000000,0.500000000000,
+                                      0.750000000000,0.250000000000,0.750000000000,
+                                      0.500000000000,0.500000000000,0.000000000000,
+                                      0.750000000000,0.750000000000,0.250000000000,
+                                      0.500000000000,0.500000000000,0.500000000000,
+                                      0.750000000000,0.750000000000,0.750000000000};
+
+  double coords[3*natoms]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};                                      
+
+  for (unsigned int i=0;i<natoms;++i)
+    for (unsigned int j=0;j<3;++j)      
+       for (unsigned int l=0;l<3;++l)
+          coords[3*i+j]+=cell[3*l+j]*coordsfrac[3*i+l];
+
+  //for (int i=0; i<natoms; i++)
+  //   std::cout<<"coords: "<<i<<", x: "<<coords[3*i+0]<<", y: "<<coords[3*i+1]<<", z: "<<coords[3*i+2]<<std::endl;
+
+  MDI_Send_command(">COORDS", mdi_comm);
+  MDI_Send(coords, 3*natoms, MDI_DOUBLE, mdi_comm);
+
+
+
+  //compute energy
+  double energy;
+  MDI_Send_command("<ENERGY", mdi_comm);
+  MDI_Recv(&energy, 1, MDI_DOUBLE, mdi_comm);  
+  std::cout<<"DFT free energy: "<<std::setprecision(10)<<energy<<std::endl;
+
+  //get forces
+  double forces[3*natoms];
+  MDI_Send_command("<FORCES", mdi_comm);
+  MDI_Recv(forces, 3*natoms, MDI_DOUBLE, mdi_comm); 
+  for (int i=0; i<natoms; i++)
+     std::cout<<"Force, Atomid: "<<i<<", x: "<<forces[3*i+0]<<", y: "<<forces[3*i+1]<<", z: "<<forces[3*i+2]<<std::endl;
+
+  //update coordinates
+  coords[0]=0.1;
+  coords[1]=0.1;
+  coords[2]=0.1;
+  MDI_Send_command(">COORDS", mdi_comm);
+  MDI_Send(coords, 3*natoms, MDI_DOUBLE, mdi_comm);
+
+  //compute energy
+  MDI_Send_command("<ENERGY", mdi_comm);
+  MDI_Recv(&energy, 1, MDI_DOUBLE, mdi_comm);  
+  std::cout<<"DFT free energy after COORDS update: "<<std::setprecision(10)<<energy<<std::endl;
+
+  //update cell
+  cell[0]=10.0;cell[1]=0.1;cell[2]=0.0;
+  cell[3]=0.0;cell[4]=11.0;cell[5]=0.0;
+  cell[6]=0.2;cell[7]=0.5;cell[8]=10.9146877116;  
+  MDI_Send_command(">CELL", mdi_comm);
+  MDI_Send(cell, 9, MDI_DOUBLE, mdi_comm);
+
+  //compute energy
+  MDI_Send_command("<ENERGY", mdi_comm);
+  MDI_Recv(&energy, 1, MDI_DOUBLE, mdi_comm);  
+  std::cout<<"DFT free energy after CELL update: "<<std::setprecision(10)<<energy<<std::endl;
+
   if ( my_rank == 0 ) {
     std::cout << " Engine name: " << engine_name << std::endl;
   }
